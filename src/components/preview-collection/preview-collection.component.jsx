7b8ae8f0e317195773bbc39be841399de9270021
wiktorzygadlo <54442899+wiktorzygadlo@users.noreply.github.com>
2019-11-23 16:33:54 +0000

    Added shop page, collection item and preview collection components

--- src/components/preview-collection/preview-collection.component.jsx
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CollectionItem from '../collection-item/collection-item.component.jsx';
 import './preview-collection.styles.scss';
 
 const PreviewCollection = ({ title, items }) => (
@@ -6,9 +7,9 @@ const PreviewCollection = ({ title, items }) => (
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
             {items
-            .filter((items, idx) => idx < 4)
-                .map(item => (
-                    <div key={item.id}>{item.name}</div>
+                .filter((item, idx) => idx < 4)
+                .map(({ id, ...otherItemProps }) => (
+                    <CollectionItem key={id} {...otherItemProps} />
             ))}
         </div>
     </div>
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -8,14 +8,14 @@ class ShopPage extends React.Component {
 
         this.state = {
             collections: SHOP_DATA
-        }
+        };
     }
 
     render() {
         const {collections} = this.state;
         return (<div className='shop-page'>
             {
-                collections.map(({id, ...otherCollections }) => (
+                collections.map(({ id, ...otherCollections }) => (
                     <PreviewCollection key={id} {...otherCollections} />
                 ))
             }
